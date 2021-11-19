9de9f4184c6366aebbf10329a00ef0946eae9738
Andrea Giacomazzi <91422164+jackomazi@users.noreply.github.com>
2021-11-19 14:50:28 +0000

    Esercizio sui file

--- Lezione2.py
+++ Lezione2.py
@@ -1,7 +1,6 @@
 def somma(lista):
     somm = 0
-    for item in lista:
-        somm = somm + item
+    for item in lista: somm = somm + item
     print("La somma dei numeri è: {}".format(somm))
 lista_num = [1,2,3,4,10]
 somma(lista_num)
